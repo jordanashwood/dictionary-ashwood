0d32b47a8a0c7e87297f4900bb28a57604a5ca17
jordanashwood <jordanmace0124@gmail.com>
2023-11-08 21:40:38 -0500

    Set up API Integration

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
       <div className="container">
         <header className="App-header"></header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="hello" />
         </main>
         <footer className="text-center mt-5">Coded by Jordan Ashwood</footer>
       </div>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,38 +1,71 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
+  let [loaded, setLoaded] = useState(false);
+  let [definition, setDefinition] = useState(null);
+  let [photos, setPhotos] = useState([]);
 
-  function handleResponse(response) {
-    console.log(response.data[0]);
+  function handleImages(response) {
+    setPhotos(response.data.photos);
   }
 
-  function search(event) {
-    event.preventDefault();
+  function handleResponse(response) {
+    setDefinition(response.data);
+    let apiKey = "fc86a336701839ca6td30ab95o4a058a";
+    let apiUrl = `https://api.shecodes.io/images/v1/define?word=${response.data.word}&key=${apiKey}`;
+    axios
+      .get(apiUrl, { headers: { Authorization: `Bearer ${apiKey}` } })
+      .then(handleImages);
+  }
 
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  function search() {
+    let apiKey = "fc86a336701839ca6td30ab95o4a058a";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
 
-  return (
-    <div className="Dictionary">
-      <form className="d-flex justify-content-center mt-5" onSubmit={search}>
-        <input
-          className="w-50"
-          type="search"
-          onChange={handleKeywordChange}
-          placeholder="Search for a word..."
-        />
-        <submit>
-          <i class="fa-solid fa-magnifying-glass"></i>
-        </submit>
-      </form>
-    </div>
-  );
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <form
+          className="d-flex justify-content-center mt-5"
+          onSubmit={handleSubmit}
+        >
+          <input
+            className="form-control w-50"
+            type="search"
+            onChange={handleKeywordChange}
+            placeholder="Search for a word..."
+            defaultValue={props.defaultKeyword}
+          />
+          <submit onSubmit={handleSubmit}>
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </submit>
+        </form>
+        <Results definition={definition} />
+        <Photos photos={photos} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading..";
+  }
 }
